77ca04b9417c09f8bcdf904eacbc12fb4fa42449
hlavacek9a@gymbk.local <hlavacek9a@gymbk.local>
2018-02-14 13:44:18 +0100

    implement gameChallengeDecline

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const moment = require('moment');
+const uuid = require('uuid/v4');
 
 const PORT = 3000; // server port
 
 const Client = function(socket) {
-  this.name = null;  // unique name of the client
-  this.socket = socket;  // socket he has connected from
+  this.name = null; // unique name of the client
+  this.socket = socket; // socket he has connected from
   this.nationality = null; // One of the Client.nationalities's items
-  this.room = null;  // null, lobby
-  this.opponent = null;  // who wants to test strength again this little fellow?
+  this.room = null; // null, lobby
+  this.opponent = null; // who wants to test strength again this little fellow?
+  this.challengedBy = [];
 
   /**
    *  Emits clientĹist with list of all current clients in lobby
@@ -18,7 +20,7 @@ const Client = function(socket) {
   this.sendClientsList = () => {
     let emittedData = []
     for (let client of clients)
-      if ((client !== this) &&(client.room === 'lobby'))
+      if ((client !== this) && (client.room === 'lobby'))
         emittedData.push({
           'name': client.name,
           'nationality': client.nationality
@@ -32,25 +34,88 @@ const Client = function(socket) {
   this.switchRoom = (room) => {
     if (this.room !== null)
       this.socket.leave(room);
-    if (room === 'lobby') // entering lobby
+
+    // Entering the lobby
+    if (room === 'lobby') {
       io.sockets.in(room).emit('clientJoinedLobby', {
         'name': this.name,
         'nationality': this.nationality
       });
-    if (this.room == 'lobby') // leaving lobby
+
+      // Clients wants to challenge somebody
+      this.socket.on('gameChallenge', (opponentName) => {
+        let opponent = Client.clientByName(opponentName, room);
+        if (opponent === null){
+          this.socket.emit('gameChallengeDecline', data);
+          return;
+        }
+        // Asked opponent is in lobby, send him a challenge message with our name
+        opponent.socket.emit('gameChallenge', this.name);
+      });
+
+      // This stranger is not worth of my time, do not battle him
+      this.socket.on('gameChallengeDecline', (notWorthyMyAttentionName) => {
+        let opponent = Client.clientByName(notWorthyMyAttentionName, room);
+        if (opponent === null)
+          return;
+        opponent.socket.emit('gameChallengeDecline', this.name);
+      });
+
+      // This seems like an interesting challenge!
+      this.socket.on('gameChallengeAccept', (opponentName) => {
+        let opponent = Client.clientByName(opponentName, room);
+        if (opponent === null)
+          return;
+        let battleRoomName = "";
+        while (true){
+          battleRoomName = "battle-" + uuid();
+          // TODO Finish joining the fight
+        }
+      });
+    }
+
+    // Leaving the lobby
+    if (this.room == 'lobby') {
+      // this.socket.off('gameChallenge'); // FIXME Why does this not work?
       io.sockets.in(this.room).emit('clientLeftLobby', {
         'name': this.name,
         'nationality': this.nationality
       });
+      this.declineAllChallenges();
+    }
     this.room = room;
     if (room !== null)
       this.socket.join(room);
+  },
+
+  /**
+   * Declines all pending challenges from other players
+   * @return {undefined}
+   */
+  this.declineAllChallenges = ()=>{
+    for (let client of this.challengedBy)
+      client.socket.emit('gameChallengeDecline', this.name);
   }
 }
 
-Client.nationalities = ["CZ", 'DE'];  // the two competing sides
+Client.nationalities = ["CZ", 'DE']; // the two competing sides
 
-const clients = [];  // list of all conected Client instances
+/**
+ * Finds client by its name in a room and returns him
+ * @param  {string} clientName name of a desired client
+ * @param  {string} room       in which room are we looking for
+ * @return {Client}            returns him as an object if found, null otherwise
+ */
+Client.clientByName = function(clientName, room) {
+  for (let client of clients)
+    if (clientName === client.name)
+      return client.room === room ? client : null;
+  return null;
+};
+
+const clients = []; // list of all conected Client instances
+
+const battles = {};
 
 app.get('*', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -108,7 +173,7 @@ io.on('connection', function(socket) {
       .map(client => client.name)
       .filter(client => client !== null)
       .join(', ')}\n`);
-  });  // End of .on('setProfile' event
+  }); // End of .on('setProfile' event
 
   /*
   It is sad, but the socket left us alone. Let's tell everybody the name of the One who left our party. (Or say nothing, if we didn't know his name)
